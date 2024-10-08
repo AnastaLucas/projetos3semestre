daa84f044309dd9c6ed7a5f1f37e502146306c2e
“LuscaAnasta” <lucasanastacio140@gmail.com>
2024-10-08 19:10:24 -0300

    interfaces

---
+++ Aula8Interfaces/src/pacote/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pacote;
+
+/**
+ *
+ * @author lucas.amsantos4
+ */
+public class Calculadora implements Operacoes{
+
+    @Override
+    public double calcularMedia(double n1, double n2) {
+        return (n1+n2/2);
+    }
+
+}
---
+++ Aula8Interfaces/src/pacote/Operacoes.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pacote;
+
+/**
+ *
+ * @author lucas.amsantos4
+ */
+public interface Operacoes {
+    abstract double calcularMedia(double n1, double n2);
+}
